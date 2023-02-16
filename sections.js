52ad0cfc028bebc90cd179e8b71cd8cc33efeeb4
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-02-16 14:26:43 +0300

    feat: add transition for section change

--- sections.js
+++ sections.js
@@ -7,6 +7,7 @@ const sections = (function(){
     })
     return resObj
 })()
+const activeClass = 'section_active'
 
 let activeSection
 
@@ -15,11 +16,41 @@ updateSection()
 window.onhashchange = updateSection
 
 function updateSection() {
-    const newHash = location.hash
-    const sectionName = newHash.replace('#', '') || 'home'
-    const section = sections[sectionName]
-    const activeClass = 'section_active'
-    if (activeSection) activeSection.classList.remove(activeClass);
-    section.classList.add(activeClass)
-    activeSection = section
+    debugger
+    document.addEventListener('transitionend', show, {once: true})
+    document.body.classList.add('body_sections-hidden')    
+    
+    function show(){
+        if (activeSection) {
+            activeSection.classList.remove(activeClass)
+        }
+        
+        const newHash = location.hash
+        const sectionName = newHash.replace('#', '') || 'home'
+        const section = sections[sectionName]
+        section.classList.add(activeClass)
+        activeSection = section
+        debugger
+        setTimeout(function(){document.body.classList.remove('body_sections-hidden')}, 0)
+    }
 }
+
+
+// const test = document.querySelector('.test')
+// document.querySelector('.test-button_opacity').addEventListener('click', function(){
+//     // test.classList.toggle('test_display')
+//     test.classList.toggle('test_hidden')
+// })
+// document.querySelector('.test-button_display').addEventListener('click', function(){
+//     test.classList.toggle('test_display')
+// })
+
+// document.querySelector('.test-button_on').addEventListener('click', function(){
+//     test.classList.toggle('test_display')
+//     test.classList.toggle('test_hidden')
+// })
+// document.querySelector('.test-button_off').addEventListener('click', function(){
+//     test.classList.toggle('test_hidden')
+//     test.addEventListener('transitionend', )
+//     test.classList.toggle('test_display')
+// })
