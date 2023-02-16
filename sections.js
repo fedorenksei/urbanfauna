1af654b2205e4994af88e1ff31b565795beb9c85
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-02-16 14:29:54 +0300

    feat: remove debugger

--- sections.js
+++ sections.js
@@ -16,7 +16,7 @@ updateSection()
 window.onhashchange = updateSection
 
 function updateSection() {
-    debugger
+    
     document.addEventListener('transitionend', show, {once: true})
     document.body.classList.add('body_sections-hidden')    
     
@@ -30,27 +30,7 @@ function updateSection() {
         const section = sections[sectionName]
         section.classList.add(activeClass)
         activeSection = section
-        debugger
+        
         setTimeout(function(){document.body.classList.remove('body_sections-hidden')}, 0)
     }
 }
-
-
-// const test = document.querySelector('.test')
-// document.querySelector('.test-button_opacity').addEventListener('click', function(){
-//     // test.classList.toggle('test_display')
-//     test.classList.toggle('test_hidden')
-// })
-// document.querySelector('.test-button_display').addEventListener('click', function(){
-//     test.classList.toggle('test_display')
-// })
-
-// document.querySelector('.test-button_on').addEventListener('click', function(){
-//     test.classList.toggle('test_display')
-//     test.classList.toggle('test_hidden')
-// })
-// document.querySelector('.test-button_off').addEventListener('click', function(){
-//     test.classList.toggle('test_hidden')
-//     test.addEventListener('transitionend', )
-//     test.classList.toggle('test_display')
-// })
