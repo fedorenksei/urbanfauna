f08bd87d934953d8b5e7e13ef2a7dc496f7790eb
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-03-17 21:59:13 +0300

    feat: slider (add infinite height adjustment) and layout

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,32 @@
-const slider = document.querySelector('.slider__row')
-const slides = document.querySelectorAll('.slider__slide')
-
-slider.addEventListener('click', nextSlide)
-
-let currentSlide = 1
-adjustHeight()
-
-function nextSlide() {
-    if (currentSlide >= slides.length) {
-        currentSlide = 1
-        slider.style.transform = `translate(0)`
-    } else {
-        currentSlide += 1
-        slider.style.transform = `translate(-${currentSlide - 1}00%)`
-    }
-    adjustHeight()
+class Slider {
+    
 }
+    const slider = document.querySelector('.slider__row')
+    const slides = document.querySelectorAll('.slider__slide')
 
-function adjustHeight() {
-    const image = slides[currentSlide - 1].querySelector('img')
-    if (image.complete && image.clientHeight != 0 && image.clientHeight != image.naturalHeight) {
-        console.log(image.clientHeight)
-        slider.style.height = slides[currentSlide - 1].clientHeight + 'px'
-    } else {
+    slider.addEventListener('click', nextSlide)
+
+    let currentSlide = 1
+    adjustHeight()
+
+    function nextSlide() {
+        if (currentSlide >= slides.length) {
+            currentSlide = 1
+            slider.style.transform = `translate(0)`
+        } else {
+            currentSlide += 1
+            slider.style.transform = `translate(-${currentSlide - 1}00%)`
+        }
+        adjustHeight()
+    }
+
+    function adjustHeight() {
+        const image = slides[currentSlide - 1].querySelector('img')
+        if (image.complete && image.clientHeight != 0 && image.clientHeight != image.naturalHeight) {
+            console.log(image.clientHeight)
+            slider.style.height = slides[currentSlide - 1].clientHeight + 'px'
+        } else {
+            // setTimeout(adjustHeight, '100')
+        }
         setTimeout(adjustHeight, '100')
     }
-}
