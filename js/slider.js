f5b322d4374eadad9f08cc7372415ee4433fb082
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-03-17 15:05:36 +0300

    feat: slider v1.2 (with initial height adjustment)

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,28 @@
 const slider = document.querySelector('.slider__row')
-const images = document.querySelectorAll('.slider__slide')
+const slides = document.querySelectorAll('.slider__slide')
 
 slider.addEventListener('click', nextSlide)
 
 let currentSlide = 1
-slider.style.height = images[0].clientHeight + 'px'
+adjustHeight()
 
 function nextSlide() {
-    if (currentSlide >= images.length) {
+    if (currentSlide >= slides.length) {
         currentSlide = 1
         slider.style.transform = `translate(0)`
     } else {
         currentSlide += 1
         slider.style.transform = `translate(-${currentSlide - 1}00%)`
     }
+    adjustHeight()
+}
 
-    slider.style.height = images[currentSlide - 1].clientHeight + 'px'
+function adjustHeight() {
+    const image = slides[currentSlide - 1].querySelector('img')
+    if (image.complete && image.clientHeight != 0 && image.clientHeight != image.naturalHeight) {
+        console.log(image.clientHeight)
+        slider.style.height = slides[currentSlide - 1].clientHeight + 'px'
+    } else {
+        setTimeout(adjustHeight, '100')
+    }
 }
